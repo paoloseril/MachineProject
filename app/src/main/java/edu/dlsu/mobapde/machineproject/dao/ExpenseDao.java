58377dae755c4b4da7c6edc23750603551ccb93c
paoloandreiseril <paolo_andrei02@yahoo.com>
2018-11-30 08:10:42 +0800

    Updated DAO to add methods for querying expenses by its attributes and searching an expense by its id

--- app/src/main/java/edu/dlsu/mobapde/machineproject/dao/ExpenseDao.java
+++ app/src/main/java/edu/dlsu/mobapde/machineproject/dao/ExpenseDao.java
@@ -22,20 +22,27 @@ public interface ExpenseDao {
     @Delete
     void deleteExpense(Expense expense);
 
-    @Query("select * from Expense where type = :type")
+    @Query("select e.id, e.name, e.type, e.dateTimeMillis, e.regretLevel from Expense e where name like :name")
+    List<Expense> getExpensesBy(String name);
+
+    @Query("select e.id, e.name, e.type, e.dateTimeMillis, e.regretLevel from Expense e where type = :type")
     List<Expense> getExpensesByType(String type);
 
-    @Query("select * from Expense where regretLevel = :regretLevel")
+    @Query("select e.id, e.name, e.type, e.dateTimeMillis, e.regretLevel from Expense e where regretLevel = :regretLevel")
     List<Expense> getExpensesByRegretLevel(int regretLevel);
 
     //@Query("select * from Expense where dateTimeMillis ")
     //List<Expense> filterExpensesDuring(long dateTimeMillis);
 
     // future expenses
-    @Query("select * from Expense where dateTimeMillis > :currentMillis order by dateTimeMillis desc")
+    @Query("select e.id, e.name, e.type, e.dateTimeMillis, e.regretLevel from Expense e where dateTimeMillis > :currentMillis order by dateTimeMillis desc")
     List<Expense> getFutureExpenses(long currentMillis);
 
     // expense history
-    @Query("select * from Expense where dateTimeMillis <= :currentMillis order by dateTimeMillis desc")
+    @Query("select e.id, e.name, e.type, e.dateTimeMillis, e.regretLevel from Expense e where dateTimeMillis <= :currentMillis order by dateTimeMillis desc")
     List<Expense> getPastExpenses(long currentMillis);
+
+    // when viewing a specific expense
+    @Query("select * from Expense where id == :id")
+    Expense getExpense(int id);
 }
