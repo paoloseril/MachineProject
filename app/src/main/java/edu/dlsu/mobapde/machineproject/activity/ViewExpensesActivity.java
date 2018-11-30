d21667b493578e9a1dc0300ce989d35106d39ae2
paoloandreiseril <paolo_andrei02@yahoo.com>
2018-12-01 00:23:39 +0800

    Added a bottom navigation menu! (2) :)

---
+++ app/src/main/java/edu/dlsu/mobapde/machineproject/activity/ViewExpensesActivity.java
@@ -0,0 +1,15 @@
+package edu.dlsu.mobapde.machineproject.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import edu.dlsu.mobapde.machineproject.R;
+
+public class ViewExpensesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_expenses);
+    }
+}
