64f2c6c7d691cb0afa7fffdadddbc12d119cfd7f
paoloandreiseril <paolo_andrei02@yahoo.com>
2018-12-01 13:26:20 +0800

    Updated fragments to include initialization of the recyclerviews

--- app/src/main/java/edu/dlsu/mobapde/machineproject/activity/MainActivityFragment.java
+++ app/src/main/java/edu/dlsu/mobapde/machineproject/activity/MainActivityFragment.java
@@ -5,6 +5,8 @@ import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -18,15 +20,40 @@ import java.util.Locale;
 import edu.dlsu.mobapde.machineproject.R;
 import edu.dlsu.mobapde.machineproject.converter.Converter;
 import edu.dlsu.mobapde.machineproject.database.ExpenseDatabase;
+import edu.dlsu.mobapde.machineproject.recyclerview1.FutureExpenseAdapter;
+import edu.dlsu.mobapde.machineproject.recyclerview2.PastExpenseAdapter;
 
 public class MainActivityFragment extends Fragment {
 
     private ExpenseDatabase expenseDatabase;
+    private RecyclerView expenseHistoryRecyclerArea, futureExpensesRecyclerArea;
+    private PastExpenseAdapter pastExpenseAdapter;
+    private FutureExpenseAdapter futureExpenseAdapter;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.activity_main, container, false);
+        View root = inflater.inflate(R.layout.activity_main, container, false);
+
+        expenseHistoryRecyclerArea = root.findViewById(R.id.expense_history_rarea);
+        futureExpensesRecyclerArea = root.findViewById(R.id.future_expense_rarea);
+
+        pastExpenseAdapter = new PastExpenseAdapter();
+        futureExpenseAdapter = new FutureExpenseAdapter();
+
+        expenseHistoryRecyclerArea.setLayoutManager(new LinearLayoutManager(getActivity()));
+        expenseHistoryRecyclerArea.setAdapter(pastExpenseAdapter);
+
+        futureExpensesRecyclerArea.setLayoutManager(new LinearLayoutManager(getActivity()));
+        futureExpensesRecyclerArea.setAdapter(futureExpenseAdapter);
+
+        return root;
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        expenseDatabase = ExpenseDatabase.getDatabase(getContext());
     }
 
     @Override
@@ -34,6 +61,14 @@ public class MainActivityFragment extends Fragment {
         super.onActivityCreated(savedInstanceState);
 
         ((AppCompatActivity) getActivity()).getSupportActionBar().hide();
-        expenseDatabase = ExpenseDatabase.getDatabase(getContext());
+    }
+
+    public void addExpenseEntry(View view) {
+
+
+    }
+
+    public void viewExpenseEntry(View view) {
+
     }
 }
--- app/src/main/java/edu/dlsu/mobapde/machineproject/activity/ViewExpensesFragment.java
+++ app/src/main/java/edu/dlsu/mobapde/machineproject/activity/ViewExpensesFragment.java
@@ -5,18 +5,37 @@ import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import edu.dlsu.mobapde.machineproject.R;
+import edu.dlsu.mobapde.machineproject.recyclerview1.FutureExpenseAdapter;
+import edu.dlsu.mobapde.machineproject.recyclerview2.PastExpenseAdapter;
+import edu.dlsu.mobapde.machineproject.recyclerview3.ExpensesViewAdapter;
 
 public class ViewExpensesFragment extends Fragment {
+
+    private RecyclerView expenseRecyclerView;
+    private ExpensesViewAdapter adapter;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.activity_view_expenses, container, false);
+        View root = inflater.inflate(R.layout.activity_view_expenses, container, false);
+
+        expenseRecyclerView = root.findViewById(R.id.all_expenses_rarea);
+        expenseRecyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+        expenseRecyclerView.setAdapter(adapter);
+
+        return root;
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
     }
 
     @Override
